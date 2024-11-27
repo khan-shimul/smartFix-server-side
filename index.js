0d4ec8218d804737ea538295c10e682b15dc6d3a
khan-shimul <khanshimul196@gmail.com>
2024-11-28 02:03:29 +0600

    implement-single-api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -31,6 +31,14 @@ async function run() {
     // Service Related API
     app.get('/services', async (req, res) => {
         const result = await serviceCollection.find().toArray();
+        res.send(result);
+    });
+    app.get('/service/:id', async(req, res) => {
+        
+        const id = req.params.id;
+        console.log(id)
+        const query = {_id: new ObjectId(id)};
+        const result = await serviceCollection.findOne(query);
         res.send(result);
     });
 
